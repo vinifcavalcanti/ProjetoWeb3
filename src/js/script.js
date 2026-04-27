db8355251ade84a69c02e6b7a42c9c992ee47758
Vinicius <viniciusfuentescavalcanti@gmail.com>
2026-04-27 09:05:24 -0300

    feat: metodos: trim, includes, split, join, toLowerCase

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+//Método de limpeza e validação
+
+const email = "   fiap@email.com    ";
+const emailLimpo = email.trim(); //remove espaços (Início e fim)
+// Método está sempre ligado a um objeto
+console.log(emailLimpo)
+
+//Validar: Verifica se o domínio é válido 
+
+if(emailLimpo.includes("@")){
+    console.log("Email válido.")
+}else{
+    console.log("Email inválido.")
+}
+
+//Métodos de transformação de texto (split, toLowerCase, Join)
+
+const tituloArtigo = "Aprendendo JavaScript RAIZ DEV";
+
+const texto1 = tituloArtigo.toLowerCase();
+console.log(texto1);
+
+const texto2 = tituloArtigo.split(" ");
+console.log(texto2);
+
+//Split transforma o texto em array []
+
+//O Join faz o inverso, ele junta tudo em uma unica string
+const texto3 = texto2.join(" ");
+console.log(texto3)
